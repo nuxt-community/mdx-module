2d16ecd9cc0c9b81da19662224aaa8ee6739777f
Sébastien Chopin <seb@nuxtjs.com>
2020-07-22 17:26:10 +0200

    initial commit

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,10 +1,9 @@
-const { resolve } = require('path')
+const { resolve } = require("path");
 
 module.exports = {
-  rootDir: resolve(__dirname, '..'),
-  buildDir: resolve(__dirname, '.nuxt'),
+  rootDir: resolve(__dirname, ".."),
+  buildDir: resolve(__dirname, ".nuxt"),
   srcDir: __dirname,
-  modules: [
-    { handler: require('../lib/module') }
-  ]
-}
+  components: true,
+  modules: [{ handler: require("../lib/module") }]
+};
